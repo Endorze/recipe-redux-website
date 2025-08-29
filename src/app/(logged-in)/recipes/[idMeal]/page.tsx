884f17292764cc46cdfdfda93a31d1d26c4eaf24
Endorze <alexyurihallgren@hotmail.com>
2025-08-29 13:23:03 +0200

    feat: Fetching data with mealID, can now start building the recipe page.

--- src/app/(logged-in)/recipes/[idMeal]/page.tsx
+++ src/app/(logged-in)/recipes/[idMeal]/page.tsx
@@ -1,12 +1,60 @@
+"use client"
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 type RecipeDetailsProps = {
-  params: { idMeal: string };
+  idMeal: string;
 };
 
-export default function RecipeDetails({ params }: RecipeDetailsProps) {
+const API = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+
+export default function RecipeDetails() {
+
+  const [meal, setMeal] = useState<any>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  
+  const { idMeal } = useParams();
+  console.log("idMeal", idMeal);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+
+    (async () => {
+      try {
+        const res = await fetch(`${API}${idMeal}`, { signal: controller.signal });
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        const data = await res.json();
+        const meal = data.meals[0];
+        setMeal(meal);
+      } catch (err: any) {
+        if (err.name !== "AbortError") setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+
+    return () => controller.abort();
+  }, [idMeal]);
+
+  console.log("meal", meal);
+
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Recipe ID: {params.idMeal}</h1>
-      <p>Här kan du fetcha detaljerad data för receptet med id {params.idMeal}.</p>
+      {loading && (
+        <p>Loading meal.</p>
+      )}
+
+      {meal && (
+
+        <div>
+          <h1 className="text-2xl font-bold">Recipe ID: {meal.idMeal}</h1>
+          <p>Här kan du fetcha detaljerad data för receptet med id {meal.idMeal}.</p>
+          <p>{meal.strCategory}</p>
+        </div>
+      )}
     </div>
   );
 }
