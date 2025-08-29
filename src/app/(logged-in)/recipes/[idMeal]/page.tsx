9fa8a81adaf19217423ad935ff9d4342895af3bc
Endorze <alexyurihallgren@hotmail.com>
2025-08-29 16:14:57 +0200

    feat: Displaying ingredients, directions etc.

--- src/app/(logged-in)/recipes/[idMeal]/page.tsx
+++ src/app/(logged-in)/recipes/[idMeal]/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-
-type RecipeDetailsProps = {
-  idMeal: string;
-};
+import { useEffect, useMemo, useState } from "react";
+import { extractIngredients } from "../../../../../lib/extractIngredients";
+import { useFavorites } from "@/context/favoritesContext";
+import { Star } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { ChevronLeft } from "lucide-react";
 
 const API = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 
@@ -13,9 +14,10 @@ export default function RecipeDetails() {
   const [meal, setMeal] = useState<any>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-
-  
-  const { idMeal } = useParams();
+  const ingredients = useMemo(() => extractIngredients(meal), [meal]);
+  const { isFavorite, toggleFavorite } = useFavorites();
+  const { idMeal } = useParams<{ idMeal: string }>();
+  const router = useRouter();
   console.log("idMeal", idMeal);
 
   useEffect(() => {
@@ -42,7 +44,7 @@ export default function RecipeDetails() {
   console.log("meal", meal);
 
   return (
-    <div className="p-6">
+    <div className="p-6 flex flex-col mx-auto max-w-5xl">
       {loading && (
         <p>Loading meal.</p>
       )}
@@ -50,9 +52,55 @@ export default function RecipeDetails() {
       {meal && (
 
         <div>
-          <h1 className="text-2xl font-bold">Recipe ID: {meal.idMeal}</h1>
-          <p>Här kan du fetcha detaljerad data för receptet med id {meal.idMeal}.</p>
-          <p>{meal.strCategory}</p>
+          <button className="underline cursor-pointer py-4 text-xl" onClick={() => router.back()}><ChevronLeft /> <span>Back</span></button>
+          <h3>Ingredients</h3>
+          <div className="flex flex-col md:flex-row md:gap-8">
+            <div className="flex flex-col basis-1/2">
+              <table className="w-full border-separate border-spacing-y-2 max-w-[300px]">
+                <tbody>
+                  {ingredients.map(({ ingredient, measure }, i) => (
+                    <tr key={i} className="align-top">
+
+                      <td className="py-1 pr-4 text-slate-900">
+                        {ingredient}
+                      </td>
+
+                      <td className="py-1 pl-4 text-left text-slate-700 tabular-nums whitespace-nowrap w-28">
+                        {measure?.trim() || "—"}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="md:basis-1/2 md:shrink-0 flex flex-col">
+              <img className=" h-auto" src={meal.strMealThumb} />
+              <div className="flex justify-between mt-2 gap-2">
+                <div className="flex gap-2">
+                  <span className="items-center self-start rounded-full bg-[#06D6A0] text-white font-semibold px-2 py-0.5">{meal.strArea}</span>
+                  <span className="items-center self-start rounded-full bg-[#06D6A0] text-white font-semibold px-2 py-0.5">{meal.strCategory}</span>
+                </div>
+                <div className="flex gap-2">
+                  <p>{meal.strMeal}</p>
+                  <button className="text-sm h-auto px-2 rounded bg-gray-100 hover:bg-gray-200" onClick={((e) => {
+                    e.stopPropagation();
+                    toggleFavorite(idMeal);
+                  })
+                  }
+                    aria-pressed={isFavorite(idMeal)}
+                    aria-label={isFavorite(idMeal) ? "Remove from Favorites" : "Add to favorites"}>
+                    <Star
+                      className="w-6 h-6"
+                      fill={isFavorite(idMeal) ? "#E63946" : "none"}
+                      stroke={isFavorite(idMeal) ? "#E63946" : "currentColor"}
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h4 className="mt-8">Directions</h4>
+          <p>{meal.strInstructions}</p>
         </div>
       )}
     </div>
